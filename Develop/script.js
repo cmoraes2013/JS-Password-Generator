4b3289cefc613b77ed05fb4b0c7235744e6a4711
Clarisse Moraes <clarissem95@live.com>
2019-12-02 21:27:28 -0500

    css completed

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,10 @@
 
   // we are creating 5 arrays with Upper Case, Lower Case, Special charact, #, an empty array for the password ends up being///
 
+  var upperCase = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S,T, U, V, W, X, Y, Z]
+  var lowerCase = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+  var specChar = ["!@#$%^&*(){}|"]
+
   //enter conditions//
 
   //run a loop//
