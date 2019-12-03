83788b51fc1950cf799ce170772ab89d973c1905
Clarisse Moraes <clarissem95@live.com>
2019-12-02 22:28:40 -0500

    added arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,11 @@
 
   // we are creating 5 arrays with Upper Case, Lower Case, Special charact, #, an empty array for the password ends up being///
 
-  var upperCase = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S,T, U, V, W, X, Y, Z]
-  var lowerCase = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
-  var specChar = ["!@#$%^&*(){}|"]
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var specChar = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":", "\'", "\\", "'", "$", "%", "&", "#", "_"]
+  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+  var empty = []
 
   //enter conditions//
 
