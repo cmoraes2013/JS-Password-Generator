d9a7d0fbe3af8e006f3b8677835d7b9009fc6747
Clarisse Moraes <clarissem95@live.com>
2019-12-06 19:33:29 -0500

    Added Empty Arrays

--- Develop/script.js
+++ Develop/script.js
@@ -5,40 +5,77 @@
   var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   var specChar = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^", "~", "*", "?", ":", "\'", "\\", "'", "$", "%", "&", "#", "_"]
   var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-  var empty = []
+  var password = [];
+  var passwordNew = [];
+  var securePassword = [];
 
-  //enter conditions//
 
-  //run a loop//
-  function generatePassword(){}
-
-var numChar = prompt ("how many characters would you like to get")
-if (numChar < 8){
-  alert ("minimum char count too low")
+//enter conditions//
+function generatePassword(){ 
+  passwordArray = []
+  var numChar = prompt ("how many characters would you like to get between 8-128?")
+  if (numChar < 8){
+    alert ("minimum char count too low")
+  }
+  console.log(numChar)
+var userLower = confirm ("Click OK to include LOWER CASE characters in your password")
+var userUpper = confirm ("Click Ok to include UPPER CASE characters in your password")
+var userSpec = confirm ("Click Ok to include SPECIAL CHARACTERS in your password")
+var userNum = confirm("Click Ok to include numbers in your password")
+if(userLower === true){
+  password.push(lowerCase);
 }
-console.log(numChar)
+if(userUpper === true){
+  password.push(upperCase);
+}
+if(userSpec === true){
+  password.push(specChar);
+}
+if(userNum === true){
+  password.push(numbers);
+}
+
+console.log(passwordNew)
+
+
+
+passwordNew = password.flat();
+console.log(passwordNew);
+
+//for loop
+for (i= 0; i < password; i++) {
+  console.log(Math.floor(Math.random() * passwordNew.length));
+  securePassword.push(newPassword[Math.floor(Math.random() * passwordNew.length)]);
+}
+
+return securePassword.join("");
+
+}
 
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-
+var copyBtn = document.querySelector("#copyToClipboard");
+​
+​
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
+  function writePassword() {
+    var passwordFinal = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = passwordFinal;
+    copyBtn.removeAttribute("disabled");
+    copyBtn.focus();
 }
-
-function copyToClipboard() {
-  // BONUS 
+​
+//function that makes the button copy to clipboard
+  function copyToClipboard() {
+    var copyText = document.getElementById("password");
+    copyText.select();
+    document.execCommand("copy");
+    //alerts what you just copied 
+    alert(copyText.value + " copied to clipboard")
 }
-
+​
+​
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// BONUS EVENT LISTENER
